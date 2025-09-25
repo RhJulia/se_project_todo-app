fd710ed417a61003ab491963cdccc7cf62350e33
julieReye$ <julia1724reyes@gmail.com>
2025-09-25 12:54:34 -0700

    applied all corrections, and removed old validate.js file, migrated to FormValidator class

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -10,34 +10,41 @@ class formValidator {
 
   _showInputError(inputElement, errorMessage) {
     this._errorElementId = `#${inputElement.id}-error`;
-    this._errorElement = this._formEl.querySelector(errorElementId);
+    this._errorElement = this._formEl.querySelector(this._errorElementId);
     inputElement.classList.add(this._inputErrorClass);
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add(this._errorClass);
+    this._errorElement.textContent = errorMessage;
+    this._errorElement.classList.add(this._errorClass);
   }
 
   _hideInputError(inputElement) {
     this._errorElementId = `#${inputElement.id}-error`;
-    this._errorElement = this._formEl.querySelector(errorElementId);
+    this._errorElement = this._formEl.querySelector(this._errorElementId);
     inputElement.classList.remove(this._inputErrorClass);
-    errorElement.classList.remove(this._errorClass);
-    errorElement.textContent = "";
+    this._errorElement.classList.remove(this._errorClass);
+    this._errorElement.textContent = "";
   }
 
   _checkInputValidity(inputElement) {
     if (!inputElement.validity.valid) {
-      this._showInputError(
-        formElement,
-        inputElement,
-        inputElement.validationMessage,
-        settings
-      );
+      this._showInputError(inputElement, inputElement.validationMessage);
     } else {
-      this._hideInputError(formElement, inputElement, settings);
+      this._hideInputError(inputElement);
     }
   }
 
-  _toggleButtonState() {}
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => !inputElement.validity.valid);
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvaldInput()) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  }
 
   _setEventListeners() {
     this._inputList = Array.from(
